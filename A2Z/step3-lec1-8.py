3e9347cded8925e8304e2e0784f6d6a169132d89
SRI-VARDHAN-ADIKI <2300033389@kluniversity.in>
2025-01-29 22:41:54 +0530

    added linear search solution

---
+++ A2Z/step3-lec1-8.py
@@ -0,0 +1,8 @@
+class Solution:
+    ##Complete this function
+    def searchInSorted(self,arr, k):
+        #Your code here
+        for i in arr:
+            if i == k:
+                return True
+        return False
