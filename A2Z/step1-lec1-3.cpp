75eec567797753396fe01a0db2b4e11a2c1e1687
SRI VARDHAN ADIKI <162698242+SRI-VARDHAN-ADIKI@users.noreply.github.com>
2025-08-27 22:54:38 +0530

    Update step1-lec1-3.cpp

--- A2Z/step1-lec1-3.cpp
+++ A2Z/step1-lec1-3.cpp
@@ -2,7 +2,7 @@ class Solution {
   public:
     string compareNM(int n, int m){
         // code here
-        if (n<m)
+        if (n<(m-1+1))
             return "lesser";
         else if (n>m)
             return "greater";
