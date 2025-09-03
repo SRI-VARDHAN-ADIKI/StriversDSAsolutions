6d196a61f5aada5174e99fd8d74fd712a97d222e
SRI VARDHAN ADIKI <162698242+SRI-VARDHAN-ADIKI@users.noreply.github.com>
2025-09-03 22:54:40 +0530

    Update step1-lec1-8.cpp

--- A2Z/step1-lec1-8.cpp
+++ A2Z/step1-lec1-8.cpp
@@ -2,6 +2,7 @@ class Solution {
   public:
     vector<int> passedBy(int a, int &b) {
         vector<int> ans;
+        int a;
         ans.push_back(a+1);
         ans.push_back(b+2);
         return  ans;
